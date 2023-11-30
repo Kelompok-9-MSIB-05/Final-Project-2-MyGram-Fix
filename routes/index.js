00a727c652d14c0273f1c03900212ab194a984af
klinkzsweet <klinkzsweet@gmail.com>
2023-11-30 20:02:44 +0700

    membuat-table-socialmedia

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router()
 
 const userRoutes = require("./UserRoutes")
-
+const socialMediaRoutes = require("./SocialMediaRoutes")
 
 
 
 router.use("/users", userRoutes)
+router.use("/socialmedias", socialMediaRoutes)
+
 
 module.exports = router
